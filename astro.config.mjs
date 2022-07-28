8bbbf7ab293a82969d06b92f2cea7298485d5b3e
Leonardo de Albuquerque Gouveia <leogouv@gmail.com>
2022-07-27 22:08:39 -0300

    Add astro-seo and astro-imagetools.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
+import { astroImageTools } from "astro-imagetools";
 
 // https://astro.build/config
 export default defineConfig({
-
+    integrations: [astroImageTools]
 });
